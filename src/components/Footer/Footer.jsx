c9cae9a1cfe9842f741339b772aa2b060b894b3f
Nadein Michael <nadein.michael@gmail.com>
2023-09-26 14:56:38 +0700

    fix: loader

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,32 +2,32 @@ import './Footer.css';
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer__container container">
-        <div className="footer__content">
-          <p className="footer__text">
+    <footer className='footer'>
+      <div className='footer__container container'>
+        <div className='footer__content'>
+          <p className='footer__text'>
             Учебный проект Яндекс.Практикум х BeatFilm.
           </p>
         </div>
-        <div className="footer__links">
-          <p className="footer__year">&copy; 2020</p>
-          <ul className="footer__list">
-            <li className="footer__list-item">
+        <div className='footer__links'>
+          <p className='footer__year'>&copy; 2023</p>
+          <ul className='footer__list'>
+            <li className='footer__list-item'>
               <a
-                className="footer__link link"
-                href="https://praktikum.yandex.ru/"
-                target="_blank"
-                rel="noreferrer"
+                className='footer__link link'
+                href='https://praktikum.yandex.ru/'
+                target='_blank'
+                rel='noreferrer'
               >
                 Яндекс.Практикум
               </a>
             </li>
-            <li className="footer__list-item">
+            <li className='footer__list-item'>
               <a
-                className="footer__link link"
-                href="https://github.com/"
-                target="_blank"
-                rel="noreferrer"
+                className='footer__link link'
+                href='https://github.com/'
+                target='_blank'
+                rel='noreferrer'
               >
                 Github
               </a>
